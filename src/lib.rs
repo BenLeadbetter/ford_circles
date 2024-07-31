885cc15ef322d64c0c03c50f8e0daedf259c19fd
Ben Leadbetter <ben.leadbetter@native-instruments.com>
2024-07-31 11:21:52 +0200

    feat: render full circle set with colour scheme

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,24 @@ use wasm_bindgen::prelude::*;
 type Matrix = nalgebra::base::Matrix3<f64>;
 type Vector = nalgebra::base::Vector2<f64>;
 
+// based on the nightfly vim colourscheme
+// https://github.com/bluz71/vim-nightfly-colors
+const BACKGROUND_COLOUR: &str = "#011627";
+const CIRCLE_COLOURS: [&str; 12] = [
+    "#FC514F",
+    "#A1CD5E",
+    "#E3D18A",
+    "#82AAFF",
+    "#C792EB",
+    "#7FDBCA",
+    "#FF5874",
+    "#22C7A8",
+    "#FFCB8B",
+    "#86BCFF",
+    "#AE81FF",
+    "#F88C6C",
+];
+
 #[derive(Debug, Default)]
 struct CanvasSize {
     width: f64,
@@ -97,13 +115,28 @@ pub fn render(
 
 pub fn draw(state: &State, context: &web_sys::CanvasRenderingContext2d) -> Result<(), JsValue> {
     let (x_min, x_max) = visible_range(state);
-    web_sys::console::debug_1(&format!("draw {x_min} to {x_max}").into());
-    let mut x = x_min.ceil() - 1.0;
-    while x < x_max + 1.0 {
-        draw_circle(context, x, 1.0)?;
-        x += 1.0;
+    let q_max = calc_q_max(x_min, x_max);
+
+    web_sys::console::debug_1(&format!("draw {x_min} to {x_max}. q_max: {q_max}").into());
+
+    for q in 1..q_max {
+        let p_min = (x_min * q as f64).floor() as i64;
+        let p_max = (x_max * q as f64).ceil() as i64;
+        for p in p_min..p_max {
+            use num_integer::Integer;
+            if p.gcd(&q)  == 1 {
+                draw_circle(context, p, q)?;
+            }
+        }
     }
+
     Ok(())
+}
+
+fn calc_q_max(x_min: f64, x_max: f64) -> i64 {
+    const MIN_CIRCLE_RADIUS_SCREEN_RATIO: f64 = 0.01;
+    let r_min = (x_max - x_min - 1.0) * 0.5 * MIN_CIRCLE_RADIUS_SCREEN_RATIO;
+    (1.0 / (2.0 * r_min)).sqrt().ceil() as i64
 }
 
 fn visible_range(state: &State) -> (f64, f64) {
@@ -119,14 +152,15 @@ fn visible_range(state: &State) -> (f64, f64) {
     (data[0] - 0.5, data[3] + 0.5)
 }
 
-fn draw_circle(context: &web_sys::CanvasRenderingContext2d, p: f64, q: f64) -> Result<(), JsValue> {
-    // web_sys::console::debug_1(&format!("draw circle: ({p}, {q})").into());
+fn draw_circle(context: &web_sys::CanvasRenderingContext2d, p: i64, q: i64) -> Result<(), JsValue> {
+    let color = CIRCLE_COLOURS[(q as usize + 2) % 12];
+    let p = p as f64;
+    let q = q as f64;
     let x = p / q;
     let r = 1.0 / (2.0 * q * q);
     context.begin_path();
     context.arc(x, r, r, 0.0, std::f64::consts::TAU)?;
-    // todo: a colour scheme
-    context.set_fill_style(&JsValue::from_str("red"));
+    context.set_fill_style(&JsValue::from_str(color));
     context.fill();
     context.close_path();
     Ok(())
@@ -143,6 +177,7 @@ fn set_transform_to_context(
 
 fn clear(state: &State, context: &web_sys::CanvasRenderingContext2d) -> Result<(), JsValue> {
     context.set_transform_with_default_dom_matrix_2d_init()?;
-    context.clear_rect(0.0, 0.0, state.canvas_size.width, state.canvas_size.height);
+    context.set_fill_style(&JsValue::from_str(BACKGROUND_COLOUR));
+    context.fill_rect(0.0, 0.0, state.canvas_size.width, state.canvas_size.height);
     Ok(())
 }
